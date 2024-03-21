36ca261f8772ab96cca81c2fbf425b5060562812
Maksim Glazunov <magus.mgl@yandex.ru>
2024-03-21 20:44:14 +0500

    Update Program.cs
    убрал комментарии

--- MySolution/Program.cs
+++ MySolution/Program.cs
@@ -4,11 +4,6 @@ using MyNamespace;
 Console.WriteLine("Программа калькулятор");
 Console.WriteLine("Введите выражение для вычисления:");
 
-// Expression correctExp = ReadСonsole.GetExpression();
-// int answer = Calculator.GetResultCalculation(correctExp.Number1, correctExp.Number2, correctExp.Operation);
-// Console.WriteLine($"{correctExp} = {answer}");
-
-
 var polishNotationExpression = ReadСonsole.GetExpressionInPolishNotation();
 var resultExpression = Calculator.GetResultExpressionInPolishNotation(polishNotationExpression);
 Console.WriteLine(resultExpression);
